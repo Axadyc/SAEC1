0e8e768f45be8a680e33df86074c657d434b513e
BEAUREPAIRE Paul <paul.beaurepaire@etu.univ-littoral.fr>
2023-09-11 17:32:41 +0200

    mise a jour du plateau + réorganisation des dossiers

--- plateau/Plateau.py
+++ plateau/Plateau.py
@@ -37,15 +37,22 @@ class Carte:
 if __name__ == "__main__":
 
     pygame.init()  
-    largeur_fenetre = 1152  # Largeur de la fenêtre en pixels 
-    hauteur_fenetre = 768  # Hauteur de la fenêtre en pixels
+    largeur_fenetre = 640  # Largeur de la fenêtre en pixels 
+    hauteur_fenetre = 640  # Hauteur de la fenêtre en pixels
     fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))  # Créer une fenêtre pygame
 
     # Dictionnaire d'images
     images = {
-        'v': pygame.image.load('python/tank.png'),
-        'w': pygame.image.load('python/fond.jpg'),
-        'r': pygame.image.load('python/ciel.jpg')
+        's': pygame.image.load('img/solsaloon.png'),
+        'w': pygame.image.load('img/solcour.jpeg'),
+        'c': pygame.image.load('img/cactus.png'),
+        'r' : pygame.image.load('img/solranch.jpg'),
+        'm' : pygame.image.load('img/solmine.jpg'),
+        '1' : pygame.image.load('img/d1.png'),
+        '2' : pygame.image.load('img/d2.png'),
+        '3' : pygame.image.load('img/d3.png'),
+        '4' : pygame.image.load('img/d4.png'),
+        'n' : pygame.image.load('img/espace.png'),
     }
 
     # # Redimensionne les image pour qu'elles ne dépassent pas 64x64 pixels
@@ -53,10 +60,14 @@ if __name__ == "__main__":
     #     images[lettre] = pygame.transform.scale(image, (64, 64))
 
     # Créer une instance de la classe Carte
-    carte = Carte(18, 12, 64, images)
+    carte = Carte(20, 20, 32, images)
+
+    # image de fond de la fenetre
+    fond = pygame.image.load('img/lesbg.jpg')
+    fond = pygame.transform.scale(fond, (largeur_fenetre, hauteur_fenetre)) # redimenssionne la taille de l'image en fonction de celle de la fenetre
 
     # Charger la carte à partir de ma_carte.txt
-    carte.charger_carte('python/ma_carte.txt')
+    carte.charger_carte('plateau/ma_carte.txt')
 
     running = True
     while running:
@@ -64,9 +75,30 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
 
-        fenetre.fill((0, 0, 0)) # couleir de fond noir
+        fenetre.blit(fond, (0,0)) # image de fond 
         carte.dessiner(fenetre) 
 
         pygame.display.update()  # Met à jour la fenêtre
 
     pygame.quit() 
+
+# cccccccccccccccccccc
+# ccmmmmmmmmmmmmmmmmcc
+# cwcmmmmmmmmmmmmmmcrc
+# cwwcmmmmmmmmmmmmcrrc
+# cwwwcmmmmmmmmmmcrrrc
+# cwwwwcmmmmmmmmcrrrrc
+# cwwwwwcmmmmmmcrrrrrc
+# cwwwwwwcmmmmcrrrrrrc
+# cwwwwwwwcmmcrrrrrrrc
+# cwwwwwwwwccrrrrrrrrc
+# cwwwwwwwwccrrrrrrrrc
+# cwwwwwwwcsscrrrrrrrc
+# cwwwwwwcsssscrrrrrrc
+# cwwwwwcsssssscrrrrrc
+# cwwwwcsssssssscrrrrc
+# cwwwcsssssssssscrrrc
+# cwwcsssssssssssscrrc
+# cwcsssssssssssssscrc
+# ccssssssssssssssss14
+# cccccccccccccccccc23
